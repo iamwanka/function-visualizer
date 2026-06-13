61962ee37aa2b46a1ba9a3569acc8d2ade5226f5
Juan Camilo Guzmán <juan.guzman@udla.edu.co>
2026-06-13 02:23:22 -0500

    add progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import {useRef, useState} from 'react'
 import './App.css'
 
+// Math evaluation - using Function constructor for simplicity, but consider using a library like math.js for more complex expressions and security
+const evaluateFunction = (expr: string, x: number, variables: Record<string, number>) =>{
+  const fn = new Function('x', ...Object.keys(variables), `return ${expr}`);
+  return fn(x, ...Object.values(variables));
+};
+
 function App() {
-  
+  // Is the same effect as getContext('2d') but with type safety
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [exp, setExp] = useState<string>('a * Math.sin(x)');
+  const [a, setA] = useState<number>(1);
+
+  // Drawing function (same logic as Vanilla)
+  const drawGraph = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    const width = canvas.width, height = canvas.height;
+    const xMin = -5, xMax = 5;
+    const yMin = -5, yMax = 5;
+
+    const toCanvasX = (x: number) => ((x - xMin) / (xMax - xMin)) * width;
+    const toCanvasY = (y: number) => height - ((y - yMin) / (yMax - yMin)) * height;
+  };
 
   return (
     <div className="App">
